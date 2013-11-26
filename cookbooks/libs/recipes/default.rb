32ff97ff34780052350452969e3b4ec4e05a0957
jblender <jblender@gmail.com>
2013-11-26 16:44:45 -0500

    Added timezone

--- cookbooks/libs/recipes/default.rb
+++ cookbooks/libs/recipes/default.rb
@@ -1,4 +1,4 @@
-%w{gcc gcc-c++ make autoconf perl-DBD-MySQL glibc-headers glibc-devel kernel-headers pcre-devel perl-XML-Parser cpp glib2-devel nc bind-utils}.each do |pkg|
+%w{gcc gcc-c++ make autoconf bind-utils}.each do |pkg|
   package pkg do
     action :install
   end
